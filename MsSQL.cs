14554d5c75d5573f92bbe95e75fd6a31c5297875
Dawwie <32976041+Dawwie@users.noreply.github.com>
2018-07-10 11:22:28 +0200

    Add files via upload
    Program do pobierania baz w mssql i mysql (C#)

---
+++ MsSQL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DBController
+{
+    class MsSQL
+    {
+        public static void Main(string[] args)
+        {
+            string connect = "Server=mssql-2016.labs.wmi.amu.edu.pl;Database=dbad_s434804;Uid=s434804;Pwd=<haslo>";
+            SqlConnection con = new SqlConnection(connect);
+
+            con.Open();
+            SqlCommand command = new SqlCommand("SELECT imie,nazwisko,dyzur FROM Nauczyciele",con);
+            SqlDataReader reader = command.ExecuteReader();
+            Console.WriteLine("IMIE" + " " + "NAZWISKO" + " " + "DYŻUR");
+            while (reader.Read())
+            {
+                string name = (string)reader["imie"];
+                string surname = (string)reader["nazwisko"];
+                string duty = (string)reader["dyzur"];
+                Console.WriteLine(name + " " + surname + " " + duty);
+            }
+            reader.Close();
+            command.Dispose();
+            con.Close();
+        }
+    }
+}
---
+++ MySQL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace DBController
+{
+    class MySQL
+    {
+        public static void Main(string[] args)
+        {
+            string connect = "Server=mysql.wmi.amu.edu.pl;Database=s434804_Szkola_Muzyczna;Uid=s434804;Pwd=<haslo>;SslMode=none";
+            MySqlConnection con = new MySqlConnection(connect);
+
+            con.Open();
+            MySqlCommand command = con.CreateCommand();
+            command.CommandText = "SELECT Imie,Nazwisko,Instrument FROM SzkolaMuzyczna";
+
+            IDataReader reader = command.ExecuteReader();
+            Console.WriteLine("IMIE" + " " + "NAZWISKO" + " " + "INSTRUMENT");
+            while (reader.Read())
+            {
+                string name = (string)reader["Imie"];
+                string surname = (string)reader["Nazwisko"];
+                string instrument = (string)reader["Instrument"];
+                Console.WriteLine(name + " " + surname + " " + instrument);
+            }
+            reader.Close();
+            command.Dispose();
+            con.Close();
+        }
+    }
+}
